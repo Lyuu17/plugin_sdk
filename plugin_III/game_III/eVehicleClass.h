d931a24435ef2475dd85ca246be36a16d294401e
Lyuu17 <14199521+Lyuu17@users.noreply.github.com>
2020-05-10 14:27:16 +0200

    GTA3/Add eCamMode and eVehicleClass enums

---
+++ plugin_III/game_III/eVehicleClass.h
@@ -0,0 +1,17 @@
+/*
+    Plugin-SDK (Grand Theft Auto 3) header file
+    Authors: GTA Community. See more here
+    https://github.com/DK22Pac/plugin-sdk
+    Do not delete this comment block. Respect others' work!
+*/
+#pragma once
+
+enum eVehicleClass
+{
+  VEHICLECLASS_CAR = 0x0,
+  VEHICLECLASS_BOAT = 0x1,
+  VEHICLECLASS_TRAIN = 0x2,
+  VEHICLECLASS_HELI = 0x3,
+  VEHICLECLASS_PLANE = 0x4,
+  VEHICLECLASS_BIKE = 0x5,
+};
